b4fec3a3b0a1950cbcc9d1bc27621d24ddfbb2b9
srvnk <105205717+srvnk@users.noreply.github.com>
2022-07-19 12:59:04 +0530

    Update misaligned.cs

--- misaligned/misaligned.cs
+++ misaligned/misaligned.cs
@@ -8,17 +8,92 @@ namespace MisalignedSpace {
         List<string> colorCodePairList = new List<string>();
         string[] colorCodePairArray = { };
         string[] actualColorCodePairArray = { };
-        
+
+        static int getLongestMajorColorString(string[] majorColor)
+        {
+            string majorColorLongString = string.Empty;
+            for (int i = 1; i < majorColor.Length; i++)
+            {
+                if(majorColor[i].Length > majorColor[i-1].Length)
+                {
+                    majorColorLongString = majorColor[i];
+                }
+                else
+                {
+                    majorColorLongString = majorColor[i-1];
+                }
+            }
+
+            return majorColorLongString.Length;
+        }
+
+        static int getLongestMinorColorString(string[] minorColor)
+        {
+            string minorColorLongString = string.Empty;
+            for (int i = 1; i < minorColor.Length; i++)
+            {
+                if (minorColor[i].Length < minorColor[i - 1].Length)
+                {
+                    minorColorLongString = minorColor[i];
+                }
+                else
+                {
+                    minorColorLongString = minorColor[i - 1];
+                }
+            }
+
+            return minorColorLongString.Length;
+        }
+
+        static string addBlankSpace(int blankSpace, string line)
+        {
+            for (int i = 0; i < blankSpace; i++)
+            {
+                line = line + " ";
+            }
+            return line;
+        }
+
+        static string[] correctMisalignedString(string[] array, int longestArrayLength)
+        {
+            List<string> newArray = new List<string>();
+            foreach(string arr in array)
+            {
+                int lengthDiff = longestArrayLength - arr.Length;
+                newArray.Add(addBlankSpace(lengthDiff, arr));
+            }
+            return newArray.ToArray();
+        }
+
         static int printColorMap() {
             string[] majorColors = {"White", "Red", "Black", "Yellow", "Violet"};
             string[] minorColors = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+            int longestMajorColorString = getLongestMajorColorString(majorColors);
+            int longestMinorColorString = getLongestMajorColorString(minorColors);
+
+            majorColors = correctMisalignedString(majorColors, longestMajorColorString);
+            minorColors = correctMisalignedString(minorColors, longestMinorColorString);
+
             int i = 0, j = 0;
-            for(i = 0; i < 5; i++) {
-                for(j = 0; j < 5; j++) {
-                    Console.WriteLine("{0} | {1} | {2}", i * 5 + j, majorColors[i], minorColors[i]);
+            for(i = 0; i < majorColors.Length; i++) {
+                for(j = 0; j < minorColors.Length; j++) {
+                    //Console.WriteLine("{0} | {1} | {2}", i * 5 + j, majorColors[i], minorColors[i]);
+                    int longestColorPairNumber = (majorColors.Length * minorColors.Length).ToString().Length;
+                    int pairNumber = ((i * (minorColors.Length)) + j + 1).ToString().Length;
+
+                    if (longestColorPairNumber == pairNumber)
+                    {
+                        colorCodePairList.Add(string.Format("{0} | {1} | {2}", ((i * (minorColors.Length) + j) + 1), majorColors[i], minorColors[j]));
+                    }
+                    else
+                    {
+                        string newPairNumber = addBlankSpace(longestColorPairNumber - pairNumber, ((i * (minorColors.Length) + j) + 1).ToString());
+                        colorCodePairList.Add(string.Format("{0} | {1} | {2}", newPairNumber, majorColors[i], minorColors[j]));
+                    }
                 }
             }
-            return i * j;
+            return majorColors.Length * minorColors.Length;
         }
         
         public void getColorCodePairList()
@@ -26,50 +101,25 @@ namespace MisalignedSpace {
             string[] majorColors = { "White", "Red", "Black", "Yellow", "Violet" };
             string[] minorColors = { "Blue", "Orange", "Green", "Brown", "Slate" };
             int i = 0, j = 0;
-            for (i = 0; i < 5; i++)
+            for (i = 0; i < majorColors.Length; i++)
             {
-                for (j = 0; j < 5; j++)
+                for (j = 0; j < minorColors.Length; j++)
                 {
-                    colorCodePairList.Add(string.Format("{0} | {1} | {2}", i * 5 + j, majorColors[i], minorColors[i]));
+                    colorCodePairList.Add(string.Format("{0} | {1} | {2}", i * minorColors.Length + j, majorColors[i], minorColors[j]));
                 }
             }
 
-            for (i = 0; i < 5; i++)
+            for (i = 0; i < majorColors.Length; i++)
             {
-                for (j = 1; j <= 5; j++)
+                for (j = 1; j <= minorColors.Length; j++)
                 {
-                    actualColorCodePairList.Add(string.Format("{0} | {1} | {2}", i * 5 + j, majorColors[i], minorColors[i]));
+                    actualColorCodePairList.Add(string.Format("{0} | {1} | {2}", i * minorColors.Length + j, majorColors[i], minorColors[j]));
                 }
             }
             colorCodePairArray = colorCodePairList.ToArray();
             actualColorCodePairArray = actualColorCodePairList.ToArray();
         }
-        
-        public void falseColorPairTest()
-        {
-            for (int i=0; i<actualColorCodePairArray.Length; i++)
-            {
-                Debug.Assert(colorCodePairArray.Equals(actualColorCodePairArray));
-            }
-
-        }
-
-        public void printColorCodeMisalignementTest()
-        {
-            string longestColorCodePairValue = string.Empty;
-            
-            for (int i = 1; i<colorCodePairArray.Length; i++)
-            {
-                longestColorCodePairValue = colorCodePairArray[i - 1].Length > colorCodePairArray[i].Length ? colorCodePairArray[i-1] : colorCodePairArray[i];
-            }
-            
-            foreach(string colorCode in colorCodePairArray)
-            {
-                Debug.Assert(colorCode.Length.Equals(longestColorCodePairValue.Length));
-            }
-
-        }
-        
+                
         static void Main(string[] args) {
             int result = printColorMap();
             Debug.Assert(result == 25);
